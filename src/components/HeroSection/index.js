27081c9f1b9dda396ebc889e15ae725deede8cc3
Saurab Prasher <saurab015@gmail.com>
2021-06-14 16:28:49 +0530

    hero section completed

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroP,
+  HeroH1,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import Video from "../../videos/video.mp4";
+
+const Hero = () => {
+  const [hover, setHover] = React.useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+
+        <HeroP>
+          Sign up for a new account today and receive $250 in credit towards
+          your next payment.
+        </HeroP>
+
+        <HeroBtnWrapper>
+          <Button
+            primary="true"
+            dark="true"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            to="signup"
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import Hero from "../components/HeroSection";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +12,7 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       <Navbar toggleSidebar={toggleSidebar} />
+      <Hero />
     </>
   );
 };
